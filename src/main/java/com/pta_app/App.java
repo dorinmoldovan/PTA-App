abc5c16ba083834f463d41e803a6d47510530b42
dorinmoldovan <12948353+dorinmoldovan@users.noreply.github.com>
2023-06-26 22:41:42 +0300

    updated App.java

--- src/main/java/com/pta_app/App.java
+++ src/main/java/com/pta_app/App.java
@@ -1,4 +1,31 @@
 package com.pta_app;
 
+import com.pta_app.algorithm.PTA;
+import com.pta_app.algorithm.Result;
+import com.pta_app.objective_functions.OFCreator;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
 public class App {
+
+    public static KeyAdapter addKeyAdapterForDigits() {
+        KeyAdapter keyAdapter = new KeyAdapter() {
+            public void keyTyped(KeyEvent e) {
+                char c = e.getKeyChar();
+                if((c < '0') && (c != KeyEvent.VK_BACK_SPACE)) {
+                    e.consume();
+                }
+                if((c > '9') && (c != KeyEvent.VK_BACK_SPACE)) {
+                    e.consume();
+                }
+            }
+        };
+        return keyAdapter;
+    }
+
 }
